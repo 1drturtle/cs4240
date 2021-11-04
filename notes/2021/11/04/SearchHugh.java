516f174538677947d2c7c5780cfa4dda8ef83dea
Chris A <1drturtle@gmail.com>
2021-11-04 11:35:41 -0400

    comments

--- notes/2021/11/04/SearchHugh.java
+++ notes/2021/11/04/SearchHugh.java
@@ -8,22 +8,30 @@ public class SearchHugh
 {
     public static void main(String[] args)
     {
+        // check for arguments
         if (args.length < 1)
         {
             System.err.println("Invalid arguments provided");
             System.exit(1);
         }
 
+        // create path of the large words file
         Path file = Path.of("hughJass.txt");
+        // get the search string from user-in
         String search = args[0];
 
         try (
+            // create a reader for the file
             BufferedReader reader = Files.newBufferedReader(file);
+            // create a writer to write the output
             BufferedWriter writer = Files.newBufferedWriter(Path.of("output.txt"));
             ) 
         {
+            // create a stream
             reader.lines()
+                  // filter each line using the search string
                   .filter(x -> x.contains(search))
+                  // write each line to the writer (output.txt)
                   .forEach(x -> {
                     try {
                         writer.write(x + "\n");
